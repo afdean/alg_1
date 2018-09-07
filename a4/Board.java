c8f468a25f5252863df0dbff37c380b78ce25db0
Andrew Dean <afdean00@gmail.com>
2018-09-06 22:29:17 -0500

    Set up a4

--- a4/Board.java
+++ a4/Board.java
@@ -1,23 +1,57 @@
 public class Board {
 
+    private int dim;
+    private int[][] blocks;
+    private int ham = -1;
+    private int manhattan = -1;
+
     // construct a board from an n-by-n array of blocks
     // (where blocks[i][j] = block in row i, column j)
     public Board(int[][] blocks) {
-
+        dim = blocks.length;
+        this.blocks = blocks;
     }
 
     // board dimension n
     public int dimension() {
-        return 0;
+        return dim;
     }
 
     // number of blocks out of place
     public int hamming() {
-        return 0;
+        // need to check dimension then iterate through n*n to see what the deal
+        // is
+
+        if (ham >= 0) {
+            return ham;
+        }
+
+        int outOfPlace = 0;
+
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                if (blocks[i][j] == 0) {
+                    continue;
+                }
+                int correctValue = (i * dim) + j + 1;
+                if (blocks[i][j] != correctValue) {
+                    outOfPlace++;
+                }
+            }
+        }
+
+        ham = outOfPlace;
+
+        return ham;
     }
 
     // sum of Manhattan distances between blocks and goal
     public int manhattan() {
+
+        if (manhattan >= 0) {
+            return manhattan;
+        }
+
         return 0;
     }
 
@@ -28,6 +62,7 @@ public class Board {
 
     // a board that is obtained by exchanging any pair of blocks
     public Board twin() {
+        // Do not swap with a 0
         return null;
     }
 
@@ -38,15 +73,26 @@ public class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
+        // At most, 4 neighbors at most only
         return null;
     }
 
     // string representation of this board (in the output format specified below)
     public String toString() {
-        return null;
+        StringBuilder s = new StringBuilder();
+        s.append(dim + "\n");
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                s.append(String.format("%2d ", blocks[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
     }
 
     // unit tests (not graded)
     public static void main(String[] args) {
 
     }
+
+}
---
+++ a4/TestBoard.java
@@ -0,0 +1,69 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.lang.Math;
+
+public class TestBoard {
+
+    int[][] blocks1 = new int[2][2];
+    int[][] blocks2 = new int[2][2];
+    int[][] blocks3 = new int[3][3];
+    int[][] blocks4 = new int[3][3];
+
+    @Before
+    public void setUp() throws Exception {
+        int[] blocks1b = {1, 2, 0, 3};
+        int[] blocks2b = {3, 1, 0, 2};
+        int[] blocks3b = {4, 2, 3, 0, 5, 7, 6, 8, 1};
+        int[] blocks4b = {0, 8, 6, 3, 4, 7, 2, 1, 5};
+        // int[] blocks4b = {0, 8, 6, 3, 4, 7, 2, 1, 5};
+        blocks1 = convertArray(blocks1b);
+        blocks2 = convertArray(blocks2b);
+        blocks3 = convertArray(blocks3b);
+        blocks4 = convertArray(blocks4b);
+        return;
+    }
+
+    // Check if hamming returns right value
+    @Test
+    public void testHamming1() {
+        Board b = new Board(blocks1);
+        Assert.assertEquals(b.hamming(), 1);
+    }
+
+    // Check if hamming returns right value
+    @Test
+    public void testHamming2() {
+        Board b = new Board(blocks2);
+        Assert.assertEquals(b.hamming(), 3);
+    }
+
+    // Check if hamming returns right value
+    @Test
+    public void testHamming3() {
+        Board b = new Board(blocks3);
+        Assert.assertEquals(b.hamming(), 4);
+    }
+
+    // Check if hamming returns right value
+    @Test
+    public void testHamming4() {
+        Board b = new Board(blocks4);
+        Assert.assertEquals(b.hamming(), 8);
+    }
+
+    private int[][] convertArray(int[] input) {
+        double inputRoot = Math.sqrt(input.length);
+        int dim = (int) inputRoot;
+        int[][] converted = new int[dim][dim];
+
+        for (int i = 0; i < dim; i++) {
+            for (int j = 0; j < dim; j++) {
+                int correctValue = (i * dim) + j;
+                converted[i][j] = input[correctValue];
+            }
+        }
+        return converted;
+    }
+
+}
