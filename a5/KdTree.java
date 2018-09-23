ee0d76e82ae28ba4f9e353bd96d98753aa1c6fe7
Andrew Dean <afdean00@gmail.com>
2018-09-22 22:41:28 -0500

    Set up TestKdTree

--- a5/KdTree.java
+++ a5/KdTree.java
@@ -3,23 +3,69 @@ import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 
 public class KdTree {
+
+    private Node root;
+    private int size;
+
+    private static class Node {
+        private Point2D p; // the point
+        private RectHV rect; // the axis-aligned rectangle corresponding to this node
+        private Node lb; // the left/bottom subtree
+        private Node rt; // the right/top subtree
+
+        private Node(Point2D p, RectHV rect) {
+            this.p = p;
+            this.rect = rect;
+        }
+    }
+
     // construct an empty set of points
     public KdTree() {
+        size = 0;
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     // number of points in the set
     public int size() {
-        return -1;
+        return size;
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        // TODO: First, don't include duplicates by checking with contains
+        root = insert(root, p, true);
+        size++;
+    }
 
+    private Node insert(Node n, Point2D p, Boolean horizontal) {
+        // TODO: Implement rect portion
+        if (n == null) {
+            return new Node(p, null);
+        }
+        int comparison;
+        if (horizontal) {
+            if (p.x() < n.p.x()) {
+                comparison = -1;
+            } else {
+                comparison = 1;
+            }
+        } else {
+            if (p.y() < n.p.y()) {
+                comparison = -1;
+            } else {
+                comparison = 1;
+            }
+        }
+        if (comparison < 0) {
+            n.lb = insert(n.lb, p, !horizontal);
+        } else if (comparison > 0) {
+            n.rt = insert(n.rt, p, !horizontal);
+        }
+        return n;
     }
 
     // does the set contain point p?
